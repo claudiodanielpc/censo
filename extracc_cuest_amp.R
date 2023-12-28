e6d5520cf4fc457e4d7d08d0980a818629e38788
Claudio Daniel Pacheco Castro <claudio@comunidad.unam.mx>
2023-12-28 08:46:07 -0600

    Update extracc_cuest_amp.R

--- extracc_cuest_amp.R
+++ extracc_cuest_amp.R
@@ -2,17 +2,22 @@
 
 ##Borrar datos del entorno
 rm(list=ls())
+
+
+#Opción para que la descarga no se corte
+options(timeout=600)
+
 #URL del Censo 2020
 url<-"https://inegi.org.mx/contenidos/programas/ccpv/2020/microdatos/Censo2020_CA_eum_csv.zip"
 
 #Establecer directorio de trabajo y crear carpeta de extracción
-raiz=setwd("D:/")
-dir.create("censo",showWarnings = F)
-directorio=paste0(raiz,"censo/")
+raiz=setwd("C:/Users/claud/Documents/")
+dir.create("datoscenso",showWarnings = F)
+directorio=paste0(raiz,"/datoscenso/")
 setwd(directorio)
 
 #Se establece que, si no existe el archivo viviendas, se descargue el zip de la URL de INEGI
-if (!file.exists("Viviendas00.CSV")){
+if (!file.exists("Personas00.csv")){
   download.file(url,destfile = "censo.zip",quiet = FALSE, mode="wb")
 #Usaremos Winrar como opción para extracción
   winrar=shQuote("C:/Program Files/WinRAR/WinRAR")
@@ -21,4 +26,6 @@ if (!file.exists("Viviendas00.CSV")){
   #Ejecución de la extracción
   cmd=paste(winrar, ' e ',file, ' -ir!*.*','"',dest,'"',sep='')
   system(cmd)
+  unlink("datoscenso/censo.zip")
 }
+
